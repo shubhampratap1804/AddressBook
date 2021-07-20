9b423e1c124bad49c364f619d900b9f68fdda2c7
pratap1804 <pratap.1804@gmail.com>
2021-07-20 21:12:50 +0530

    Added: welcome message

---
+++ src/addressBook/com/Person.java
@@ -0,0 +1,9 @@
+package addressBook.com;
+
+public class Person {
+
+			//Main method
+			public static void main(String[] args) {
+				System.out.println("Welcome to address book program!");
+			}
+		}
