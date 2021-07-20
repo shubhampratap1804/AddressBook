593897eb8ffd319daeb9fc25d726ccf151676ff8
pratap1804 <pratap.1804@gmail.com>
2021-07-21 00:37:50 +0530

    Added: ability to add multiple person into contact list

--- src/addressBook/com/AddressBook.java
+++ src/addressBook/com/AddressBook.java
@@ -10,44 +10,50 @@ public class AddressBook {
 			//Method to add a new contact	
 			
 			  static void addContacts() {
-					
-					Person person = new Person();
 					Scanner scan = new Scanner(System.in);
 					
-					System.out.println("Enter first name: ");
-					String firstName= scan.next();
-					person.setFirstName(firstName);
+					System.out.println("How many person you want to add?");
+					int numberOfPerson = scan.nextInt();
 					
-					System.out.println("Enter last name: ");
-					String lastName= scan.next();
-					person.setLastName(lastName);
+					for(int i=0; i<numberOfPerson; i++) {
+						Person person = new Person();
+						
+						System.out.println("Enter first name: ");
+						String firstName= scan.next();
+						person.setFirstName(firstName);
+						
+						System.out.println("Enter last name: ");
+						String lastName= scan.next();
+						person.setLastName(lastName);
+						
+						System.out.println("Enter address: ");
+						String address= scan.next();
+						person.setAddress(address);
+						
+						System.out.println("Enter city: ");
+						String city= scan.next();
+						person.setCity(city);
+						
+						System.out.println("Enter state ");
+						String state= scan.next();
+						person.setState(state);
+						
+						System.out.println("Enter zip code: ");
+						String zip= scan.next();
+						person.setZip(zip);
+						
+						System.out.println("Enter phone number: ");
+						String phoneNumber= scan.next();
+						person.setPhoneNumber(phoneNumber);
+						
+						System.out.println("Enter email: ");
+						String email= scan.next();
+						person.setEmail(email);
+						contactList.add(person);
+						
+						System.out.println(person.toString());
+					}
 					
-					System.out.println("Enter address: ");
-					String address= scan.next();
-					person.setAddress(address);
-					
-					System.out.println("Enter city: ");
-					String city= scan.next();
-					person.setCity(city);
-					
-					System.out.println("Enter state ");
-					String state= scan.next();
-					person.setState(state);
-					
-					System.out.println("Enter zip code: ");
-					String zip= scan.next();
-					person.setZip(zip);
-					
-					System.out.println("Enter phone number: ");
-					String phoneNumber= scan.next();
-					person.setPhoneNumber(phoneNumber);
-					
-					System.out.println("Enter email: ");
-					String email= scan.next();
-					person.setEmail(email);
-					contactList.add(person);
-					
-					System.out.println(person.toString());
 				}
 				
 				//Method to edit a contact
