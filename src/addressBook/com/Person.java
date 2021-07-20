ebb3a5d716f06d0cbbf308c97f5a6a40353af2db
pratap1804 <pratap.1804@gmail.com>
2021-07-20 22:47:42 +0530

    Created: pojo class to create contacts

--- src/addressBook/com/Person.java
+++ src/addressBook/com/Person.java
@@ -2,6 +2,94 @@ package addressBook.com;
 
 public class Person {
 
+			// Declaring variables
+			
+			String firstName;
+			String lastName;
+			String address;
+			String city;
+			String state;
+			String zip;
+			String phoneNumber;
+			String email;
+			
+			
+			//Generated getters and setters for each instance variable
+			
+			public String getFirstName() {
+				return firstName;
+			}
+		
+			public void setFirstName(String firstName) {
+				this.firstName = firstName;
+			}
+		
+			public String getLastName() {
+				return lastName;
+			}
+		
+			public void setLastName(String lastName) {
+				this.lastName = lastName;
+			}
+		
+			public String getAddress() {
+				return address;
+			}
+		
+			public void setAddress(String address) {
+				this.address = address;
+			}
+		
+			public String getCity() {
+				return city;
+			}
+		
+			public void setCity(String city) {
+				this.city = city;
+			}
+		
+			public String getState() {
+				return state;
+			}
+		
+			public void setState(String state) {
+				this.state = state;
+			}
+		
+			public String getZip() {
+				return zip;
+			}
+		
+			public void setZip(String zip) {
+				this.zip = zip;
+			}
+		
+			public String getPhoneNumber() {
+				return phoneNumber;
+			}
+		
+			public void setPhoneNumber(String phoneNumber) {
+				this.phoneNumber = phoneNumber;
+			}
+		
+			public String getEmail() {
+				return email;
+			}
+		
+			public void setEmail(String email) {
+				this.email = email;
+			}
+
+
+			//Generated to string method
+			
+			@Override
+			public String toString() {
+				return "Person [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city="
+						+ city + ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email="
+						+ email + "]";
+			}
+
 			//Main method
 			public static void main(String[] args) {
 				System.out.println("Welcome to address book program!");
